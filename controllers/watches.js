f9056a37146ca4165a200e5b526cf1d94275e7fc
will <wbrycejohnson031086@gmail.com>
2022-03-07 10:24:04 -0500

    add watches controller and router

---
+++ controllers/watches.js
@@ -0,0 +1,9 @@
+import { Watch } from "../models/watch.js"
+
+function index(req, res) {
+  console.log('What time is it?')
+}
+
+export {
+  index,
+}
--- routes/watches.js
+++ routes/watches.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
+import * as watchesCtrl from '../controllers/watches.js'
 
 const router = Router()
 
-
+// GET - localhost/3000/watches
+router.get('/', watchesCtrl.index)
 
 export {
   router
