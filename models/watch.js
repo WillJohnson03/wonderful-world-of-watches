96e192f15c38e4a5b1d8cc7c59862f8fd3a9e1df
will <wbrycejohnson031086@gmail.com>
2022-03-07 11:58:39 -0500

    add new.ejs and update model

--- models/watch.js
+++ models/watch.js
@@ -3,10 +3,12 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const watchSchema = new Schema({
-  brand: String,
-  model: String,
-  type: String,
-  complications: String,
+  brand: {type: String, required: true},
+  model: {type: String, required: true},
+  type: {type:String, enum: ["Analog", "Digital", "Smart"]},
+  complications: {type: String, required: true},
+}, {
+  timestamps: true
 })
 
 const Watch = mongoose.model('Watch', watchSchema)
