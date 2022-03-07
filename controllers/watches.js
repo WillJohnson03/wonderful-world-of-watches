727ff24703bf109885f1f615865f0659c5bfbc75
will <wbrycejohnson031086@gmail.com>
2022-03-07 11:30:34 -0500

    implemnet add watches fuction

--- controllers/watches.js
+++ controllers/watches.js
@@ -1,9 +1,25 @@
 import { Watch } from "../models/watch.js"
 
 function index(req, res) {
-  console.log('What time is it?')
+  Watch.find({})
+  .then(watches => {
+    res.render('watches/index', {
+      watches,
+      title: 'All Watches'
+    })
+  })
+  .catch(err => {
+    res.redirect('/watches')
+  })
+}
+
+function newWatch (req, res){
+  res.render('watches/new', {
+    title: 'Add Watch'
+  })
 }
 
 export {
   index,
+  newWatch as new
 }
--- routes/watches.js
+++ routes/watches.js
@@ -1,10 +1,13 @@
 import { Router } from "express"
 import * as watchesCtrl from '../controllers/watches.js'
+import { isLoggedIn } from "../middleware/middleware.js"
 
 const router = Router()
 
 // GET - localhost/3000/watches
 router.get('/', watchesCtrl.index)
+// GET - localhost/3000/watches/new
+router.get('/new', isLoggedIn, watchesCtrl.new)
 
 export {
   router
