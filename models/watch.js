d12f7c686f702b0314e5a5dc2ef22fcd40b8b175
will <wbrycejohnson031086@gmail.com>
2022-03-17 21:19:56 -0400

    add review schema

--- models/watch.js
+++ models/watch.js
@@ -2,12 +2,20 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const reviewSchema = new Schema({
+  content: String,
+  rating: {type: Number, min: 1, max: 5, default: 5}
+}, {
+  timestamps: true
+})
+
 const watchSchema = new Schema({
   brand: {type: String, required: true},
   model: {type: String, required: true},
   type: {type:String, enum: ["Analog", "Digital", "Smart"]},
   complications: {type: String},
-  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+  reviews: [reviewSchema]
 }, {
   timestamps: true
 })
